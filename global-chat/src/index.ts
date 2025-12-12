277fc6068b8ef05ea0cac7c8088864ebfba59de4
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-12-13 00:35:51 +0530

    Add Room based chat

--- global-chat/src/index.ts
+++ global-chat/src/index.ts
@@ -1,23 +1,70 @@
-import {WebSocketServer, WebSocket} from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
-const wss = new WebSocketServer({port: 8080});
+const wss = new WebSocketServer({ port: 8080 });
 let count = 0;
 
-// storing all the socket that are getting connected 
-let websocketArray : WebSocket[] = []
-wss.on('connection',function connection(ws){
-    websocketArray.push(ws);
-    ws.on('error',console.error);
+// 2. We will go a bit complex , now the msg type will be an object. 
+interface User {
+    socket: WebSocket,
+    room: string
+}
+// storing User type inside the array. 
+let websocketArray: User[] = [];
 
-    ws.on('message',(data)=>{
-        websocketArray.forEach((s)=>{
-            s.send(data.toString()+ " Sent from the server")
-        })
+
+// 1. storing all the socket that are getting connected 
+// let websocketArray : WebSocket[] = []
+wss.on('connection', function connection(ws) {
+    // websocketArray.push(ws);
+    ws.on('error', console.error);
+
+    ws.on('message', (message) => {
+
+        let parsedMsg = JSON.parse(message as unknown as string);
+
+        // let say we have two type of message if the user is gonna join or chat 
+        // the msg type could be 
+        /*
+            { 
+                "type" : "join" , 
+                "payload" : {
+                    "room_id" : "124"
+                    }
+            }
+            
+            { 
+                "type" : "chat" , 
+                "payload" : {
+                    "message" : "Hi There!"
+                    }
+            }
+
+        based  on the type of input , we agre gonna perform the action 
+        */
+
+        if (parsedMsg.type == "join") {
+            websocketArray.push({
+                socket: ws,
+                room: parsedMsg.payload.room_id
+            })
+            console.log("User joined the" + parsedMsg.payload.room_id)
+        }
+        if (parsedMsg.type == "chat") {
+
+            let currentUserRoom = websocketArray.find((x) => x.socket = ws)?.room;
+
+            for (let i = 0; i < websocketArray.length; i++) {
+                if (websocketArray[i]?.room == currentUserRoom) {
+                    websocketArray[i]?.socket.send(parsedMsg.payload.message)
+                }
+            }
+
+        }
     })
-    count = count+1 ; 
-    console.log("User "+count)
+    count = count + 1;
+    console.log("User " + count)
 
-    ws.on('disconnect',()=>{
-      websocketArray = websocketArray.filter((x)=> x != ws)
+    ws.on('disconnect', () => {
+        websocketArray = websocketArray.filter((x) => x.socket != ws)
     })
 })
