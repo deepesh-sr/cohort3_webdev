db7b27131fc68568b9228bb0c2eea6c4cb46f190
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-12-12 19:36:02 +0530

    Add ws global chat

---
+++ global-chat/src/index.ts
@@ -0,0 +1,19 @@
+import {WebSocketServer, WebSocket} from 'ws';
+
+const wss = new WebSocketServer({port: 8080});
+let count = 0;
+
+// storing all the socket that are getting connected 
+const websocketArray : WebSocket[] = []
+wss.on('connection',function connection(ws){
+    websocketArray.push(ws);
+    ws.on('error',console.error);
+
+    ws.on('message',(data)=>{
+        websocketArray.forEach((s)=>{
+            s.send(data.toString()+ " Sent from the server")
+        })
+    })
+    count = count+1 ; 
+    console.log("User "+count)
+})
