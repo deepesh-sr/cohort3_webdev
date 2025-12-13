5fa94f734a233c54bae5256e67bfa33980522a12
deepesh-sr <220714100078@cutm.ac.in>
2025-12-14 00:05:21 +0530

    Add frontend with ws implementation like global chat.

--- global-chat-frontend/src/App.tsx
+++ global-chat-frontend/src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [msg , setMsg] = useState(["Hello everyone"])
+  const wsRef = useRef<WebSocket>(null);
+  useEffect(()=>{
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onmessage = (e) => {
+      setMsg(m => [...m,e.data])
+    }
+    ws.onopen = ()=>{
+      ws.send(JSON.stringify({
+        "type" : "join",
+        "payload" : {
+          "room_id" : "red"
+        }
+      }))
+    }
+
+    wsRef.current = ws;
+
+    return ()=> ws.close();
+
+  },[]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+
+        </div>
+        <div>
+          <input id='inputbox' type="text" placeholder='Enter your message'/>
+          <button onClick={()=>{
+            // @ts-ignore
+            const msg = document.getElementById('inputbox').value;
+            wsRef.current?.send(JSON.stringify({
+              "type" : "chat",
+              "payload" : {
+                "message" : msg
+              }
+            }))
+          }}>Send</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
