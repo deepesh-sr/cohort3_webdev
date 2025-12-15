1d60fcbc6b96216e4cbad587cabd402f9cff5778
deepesh-sr <220714100078@cutm.ac.in>
2025-12-15 20:06:05 +0530

    simple query added and tested

--- postgres-practice/src/index.ts
+++ postgres-practice/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hi There")
+import {Client} from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+// const pgClient = new Client({
+//     user : process.env.DB_USER,
+//     password : process.env.DB_PASSWORD,
+//     host : process.env.DB_HOST,
+//     database : "neondb",
+//     ssl:true,
+// })
+
+const pgClient = new Client(process.env.DS_URL);
+async function main() {
+    await pgClient.connect();
+    const response =await pgClient.query("SELECT * FROM books");
+    console.log(response.rows);
+}
+main()
